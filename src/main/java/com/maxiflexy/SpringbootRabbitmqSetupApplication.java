d448b617563bc2dba5e131cdba9828190c55c62a
Maxiflexy <onyemax247@gmail.com>
2025-02-03 14:31:12 +0100

    project set-up

---
+++ src/main/java/com/maxiflexy/SpringbootRabbitmqSetupApplication.java
@@ -0,0 +1,13 @@
+package com.maxiflexy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRabbitmqSetupApplication.class, args);
+    }
+
+}
