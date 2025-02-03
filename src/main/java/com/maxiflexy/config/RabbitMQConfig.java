b985731d71e13ae229a8b0611aa812dc39c403b0
Maxiflexy <onyemax247@gmail.com>
2025-02-03 15:16:01 +0100

    add new queue config for json

--- src/main/java/com/maxiflexy/config/RabbitMQConfig.java
+++ src/main/java/com/maxiflexy/config/RabbitMQConfig.java
@@ -14,17 +14,29 @@ public class RabbitMQConfig {
     @Value("${rabbit.queue.name}")
     private String queue;
 
+    @Value("${rabbit.queue.json.name}")
+    private String jsonQueue;
+
     @Value("${rabbit.exchange.name}")
     private String exchange;
 
     @Value("${rabbit.routing.key}")
     private String routingKey;
 
+    @Value("${rabbit.routing.json.key}")
+    private String routingJsonKey;
+
 
     // spring bean for rabbitmq queue
     @Bean
     public Queue queue(){
         return new Queue(queue);
+    }
+
+    // spring bean fpr rabbitmq queue to store json messages
+    @Bean
+    public Queue jsonQueue(){
+        return new Queue(jsonQueue);
     }
 
 
@@ -41,5 +53,12 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(queue())
                 .to(exchange())
                 .with(routingKey);
+    }
+
+    @Bean
+    public Binding jsonBinding(){
+        return BindingBuilder.bind(jsonQueue())
+                .to(exchange())
+                .with(routingJsonKey);
     }
 }
