799b48736789ca2e99ac3e25ba0874a384327616
Maxiflexy <onyemax247@gmail.com>
2025-02-03 15:07:47 +0100

    set for impl to consume a message

--- src/main/java/com/maxiflexy/consumer/RabbitMQConsumer.java
+++ src/main/java/com/maxiflexy/consumer/RabbitMQConsumer.java
@@ -1,4 +1,15 @@
 package com.maxiflexy.consumer;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class RabbitMQConsumer {
+
+    @RabbitListener(queues = "${rabbit.queue.name}")
+    public void consume(String message){
+        log.info("Received message -> {}", message);
+    }
 }
